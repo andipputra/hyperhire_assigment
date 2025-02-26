cbb1afa38002af84fe2efd16b298a2454e4ea6c1
Andi Pratama Putra <app.andipputra@gmail.com>
2025-02-26 22:07:58 +0700

    style: register colors

---
+++ lib/core/constant/color.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColor {
+  static const white = Color(0xffFFFFFF);
+  static const iris = Color(0xff5D5FEF);
+  static const basicBlack = Color(0xff1D1D1D);
+  static const black = Color(0xff000000);
+  static const gray = Color(0xff616161);
+  static const demiGray = Color(0xff868686);
+  static const lightGray = Color(0xffF0F0F0);
+  static const gold = Color(0xffFFD233);
+}
